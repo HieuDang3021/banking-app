bedf447365bcc99c7e3e423bfaf5369eaff44773
HieuDang3021 <trhieu1247@gmail.com>
2025-04-27 14:39:11 +1000

    tailwind v4 setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="flex text-24 bg-blue-25 animate-accordion-down">
+      Home
+    </section>
+  );
+};
+
+export default Home;
