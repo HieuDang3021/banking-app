d91f7f7723ff46e95975f17c69025c4ae6b5e655
HieuDang3021 <trhieu1247@gmail.com>
2025-05-02 23:43:22 +1000

    implement sidebar

---
+++ components/MobileNav.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type user = {
+  firstName: string;
+  lastName: string;
+};
+
+const MobileNav = ({ user }: user) => {
+  return <div>MobileNav</div>;
+};
+
+export default MobileNav;
